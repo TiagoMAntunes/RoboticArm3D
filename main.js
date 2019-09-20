efb4e2c6ee045a086089e9fb9274dcf2be22f04b
TiagoMAntunes <tiago.melo.antunes@tecnico.ulisboa.pt>
2019-09-20 23:38:41 +0100

    Added base top

--- main.js
+++ main.js
@@ -18,23 +18,34 @@ function createCameras() {
 
 
 function createWheel(x, y, z) {
-    var sphere = new THREE.Object3D()
     var geometry = new THREE.SphereGeometry(2,20,20)
-    let material = new THREE.MeshBasicMaterial({wireframe: true, color: 0xffff00} )
+    var material = new THREE.MeshBasicMaterial({wireframe: true, color: 0xffff00} )
     var wheel = new THREE.Mesh(geometry,material)
     wheel.position.set(x,y,z)
-    sphere.add(wheel)
-    scene.add(sphere)
+    return wheel
+}
+
+
+function createBase(x,y,z) {
+    //Base is rectangular and has a ball on its center
+    var geometry = new THREE.BoxGeometry(20,20,2)
+    var material = new THREE.MeshBasicMaterial({wireframe: true, color: 0x0000ff})
+    var mesh = new THREE.Mesh(geometry,material)
+    mesh.position.set(x,y,z)
+    return mesh
 }
 
 
 function createScene() {
     scene = new THREE.Scene()
     scene.add(new THREE.AxesHelper(5))
-    createWheel(5, 8, 2)
-    createWheel(5, 21, 2)
-    createWheel(18,8,2)
-    createWheel(18,21,2)
+    let base = new THREE.Object3D()
+    base.add(createWheel(5, 8, 2))
+    base.add(createWheel(5, 21, 2))
+    base.add(createWheel(18,8,2))
+    base.add(createWheel(18,21,2))
+    base.add(createBase(11.5,14.5,5))
+    scene.add(base)
 }
 
 //setup of scene
