10dae833f425e1692ed67ddec2979614226f9c16
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-09-29 12:15:36 +0100

    Removed test_OOP folder

--- test_OOP/Finger.js
+++
@@ -1,8 +0,0 @@
-class Finger extends SceneObject {
-    constructor(x, y, z, fx, fy, fz, width, height, depth, material) {
-        super()
-        
-        this.addSceneObjBox(this, fx, fy, fz, width, height, depth, material)
-        this.position.set(x, y, z)
-    }
-}
